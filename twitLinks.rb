bcd55a8ce51540b02c4943f3661c3d16875cf35b
Peter Heinrich <peter.heinrich@gmail.com>
2015-06-29 10:36:34 -0700

    Can now generate separate output files tracking mentions and hashtags

--- twitLinks.rb
+++ twitLinks.rb
@@ -6,6 +6,7 @@ require 'optparse'
 require 'net/http'
 require 'uri'
 
+TWEETID_COL = 0
 TWEETTEXT_COL = 2
 
 options = {}
@@ -18,6 +19,8 @@ OptionParser.new do |opts|
   opts.on( '-a', '--append FILE', 'Append output in a single file, which may exist' ) {|a| options[:append] = a}
   opts.on( '-e', '--encoding ENC', "Open source files using a specific encoding (default: #{options[:encoding]})" ) {|e| options[:encoding] = e}
   opts.on( '-h', '--help', 'Display this usage information' ) {puts opts; exit}
+  opts.on( '-m', '--mentions', 'Generate separate output file(s) tracking mentions' ) {|m| options[:mentions] = m}
+  opts.on( '-t', '--tags', 'Generate separate output file(s) tracking hashtags' ) {|t| options[:tags] = t}
   opts.on( '-v', '--[no-]verbose', 'Display extra info during execution' ) {|v| options[:verbose] = v}
   opts.on( '-x', '--extension EXT', "Use a specific extension for output files (default: #{options[:extension]})" ) {|x| options[:extension] = x}
 end.parse!
@@ -25,10 +28,12 @@ end.parse!
 # Follow an unlimited number of redirect requests to eventually reach a
 # final resource location on the web.
 def redirect( uri )
+  maxHops = 5
   location = nil
 
-  until uri.nil?
+  until uri.nil? || 0 == maxHops
     location = uri
+    maxHops -= 1
     uri = get_location( location )
   end
 
@@ -69,12 +74,13 @@ def rebase( location, uri )
 end
 
 # Construct an output path from the filename given. This amounts to adding
-# '_out' to the root filename and changing the extension, if necessary.
-def get_output_path( file, defExt )
+# '_out' (or other descriptor) to the root filename and changing the exten-
+# sion, if necessary.
+def get_output_path( file, defExt, type = 'out' )
   arr = File.split( file )
   ext = File.extname( arr[-1] )
 
-  arr[-1] = "#{File.basename( arr[-1], ext )}_out#{defExt}"
+  arr[-1] = "#{File.basename( arr[-1], ext )}_#{type}#{defExt}"
   File.join( arr )
 end
 
@@ -94,6 +100,7 @@ end
 
 # Process each file specified on the command line.
 ARGV.each do |file|
+  mentions, tags = [], []
   file += options[:extension] if File.extname( file ).empty?
   puts "Reading #{file}..."
 
@@ -105,14 +112,16 @@ ARGV.each do |file|
 
     links.each_with_index do |row, i|
       row << 'URL' if 0 == i
+      id = row[TWEETID_COL]
 
       # Eliminate carriage returns in the source data, since they contaminate
       # the output and lead to formatting errors.
       row.map! {|col| col.gsub( /\r/, '' )}
+      tweet = row[TWEETTEXT_COL]
       target = nil
   
       # Find the first link embedded in the tweet text, if any.
-      if /^.*?(?<url>https*:\/\/t\.co\/[a-zA-Z0-9]+)/.match( row[TWEETTEXT_COL] )
+      if /^.*?(?<url>https*:\/\/t\.co\/[a-zA-Z0-9]+)/.match( tweet )
         # Follow all redirects until the actual resource is found.
         url = $~[:url] 
         uri = URI.parse( redirect( url ) )
@@ -129,8 +138,32 @@ ARGV.each do |file|
         puts "  #{i}:<no links>" if options[:verbose]
       end
 
+      # Track hashtags and references to other Twitter users, if requested.
+      tweet.scan( /@(\w{1,15})/ ).flatten.each {|user| mentions << [id, user]} if options[:mentions]
+      tweet.scan( /#(\w+)/ ).flatten.each {|tag| tags << [id, tag]} if options[:tags]
+ 
       row << target
       csv << row
+    end
+  end
+
+  # Write a separate output file tracking mentions, if requested.
+  if options[:mentions]
+    out = get_output_path( file, options[:extension], 'mentions' )
+    CSV.open( out, 'ab' ) do |csv|
+      puts "Writing mentions to #{out}..."
+      csv << ['Tweet id', 'Mention']
+      mentions.each {|mention| csv << mention}
+    end
+  end
+
+  # Write a separate output file tracking hashtags, if requested.
+  if options[:tags]
+    out = get_output_path( file, options[:extension], 'hashtags' )
+    CSV.open( out, 'ab' ) do |csv|
+      puts "Writing hashtags to #{out}..."
+      csv << ['Tweet id', 'Hashtag']
+      tags.each {|tag| csv << tag}
     end
   end
 end
